7d7c86a1b2854174cb42c843ac3d793d4425e82b
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-03-19 19:35:55 -0400

    commit tree 98f3ab2e2cf0ff797c1e6d7358ddb4dbd10a02a4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const message = [];
+function writeCards(names,event) {
+    for (let i=0; i < names.length; i++) {
+            message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        }
+        return message;
+    }
+    console.log(writeCards(names, surprise));
+
+    
+    function countDown() {
+        let i = 0;
+        while (i < 11) {
+            console.log(i++)
+        }
+    }
